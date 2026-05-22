8618ce840876cc8b25ec6e5fcd203be0509180fa
user <guilhermevasconcelosdev@gmail.com>
2026-05-22 20:34:25 -0300

    feat: config melhorada, correção sobre as rotas e retorno dos tokens

--- journal/urls.py
+++ journal/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import JournalEntryViewSet, PublicJournalViewSet
 
 router = DefaultRouter()
-router.register(r'journal', JournalEntryViewSet, basename='journal-entry')
 router.register(r'journal/public', PublicJournalViewSet, basename='public-journal')
+router.register(r'journal', JournalEntryViewSet, basename='journal-entry')
 
 urlpatterns = [
     path('', include(router.urls)),
