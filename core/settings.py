29c0b02d61f4c3bffbe7e8b71c1257d56ef26442
user <guilhermevasconcelosdev@gmail.com>
2026-05-30 12:35:00 -0300

    update

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'personal-chico-security-api.onrender.com',
+]
 
 
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='', cast=Csv())
